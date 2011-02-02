bd861fce35db2a34d171039d7b62f780db257410
bartbes <bart.bes+projects@gmail.com>
2011-02-02 17:26:21 +0100

    C++ vm accepts stdin as file (using -) and script uses that now

--- vm.cpp
+++ vm.cpp
@@ -607,13 +607,24 @@ int main(int argc, const char **argv)
 		cout<<"Usage: " <<argv[0] <<" <file>\n";
 		return 0;
 	}
-	ifstream f(argv[1]);
-	f.seekg(0, ios::end);
-	int l = f.tellg();
-	f.seekg(0, ios::beg);
-	char *buffer = new char[l];
-	f.read(buffer, l);
-	f.close();
+	char *buffer;
+	istream *f;
+	if (strcmp(argv[1], "-") == 0)
+	{
+		std::stringstream *stream = new std::stringstream();
+		*stream << cin.rdbuf();
+		f = stream;
+	}
+	else
+	{
+		f = new ifstream(argv[1]);
+	}
+	f->seekg(0, ios::end);
+	int l = f->tellg();
+	f->seekg(0, ios::beg);
+	buffer = new char[l];
+	f->read(buffer, l);
+	delete f;
 	contexts.insert(0, buffer);
 	unsigned int n;
 	for (int i = 2; i < argc; i++)
