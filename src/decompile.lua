8b61131edf5f9c6126e32fb302959362fb8beb06
Bart van Strien <bart.bes+projects@gmail.com>
2011-02-28 14:06:24 +0100

    [decompiler] Add linenumbers

--- src/decompile.lua
+++ src/decompile.lua
@@ -43,9 +43,11 @@ if not i then
 	return 1
 end
 local line = i:read(3)
+local linenum = 0
 while line do
 	local a, b, c = string.byte(line:sub(1, 1)), string.byte(line:sub(2, 2)), string.byte(line:sub(3, 3))
-	print(("%02x%02x%02x    %s %02x%02x"):format(a, b, c, commandlist[a] or "unknown", b, c))
+	print(("%04x:  %02x%02x%02x    %s %02x%02x"):format(linenum, a, b, c, commandlist[a] or "unknown", b, c))
 	line = i:read(3)
+	linenum = linenum + 1
 end
 i:close()
