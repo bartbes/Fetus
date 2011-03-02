d9b201581ce8885d220912e0eb452d6b2c6bcff5
bartbes <bart.bes+projects@gmail.com>
2011-03-02 17:24:00 +0100

    [general] Create tests and test running script

---
+++ run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+log()
+{
+	echo -e "\033[;32m$*\033[;37m"
+}
+
+#example_files=`find examples/ -type f`
+example_files= #don't run examples
+test_files=`find tests/ -type f`
+
+files="$example_files $test_files"
+
+for file in $files; do
+	log "Running file: $file"
+	extension=`echo $file | sed 's/.*\.\(ft[sd]\)/\1/'`
+	case $extension in
+		fts)
+			./fetus $file
+			;;
+		ftd)
+			./fetoid $file
+			;;
+		*)
+			log "Don't recognize extension $extension"
+			;;
+	esac
+	log "Done"
+done
